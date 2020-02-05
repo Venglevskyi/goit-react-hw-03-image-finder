a41a634cd10ca42315c53fce1823c363830a0eac
Roman <Roman.Vengl1@gmail.com>
2020-02-05 15:55:07 +0200

    Updates

---
+++ precache-manifest.7661e0b78c6b4f7a87791c74d67e99a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d3b3d60b458ced0c9327adbf252b463",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "e2f3030373d2bd8b2209",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.a38f88f2.chunk.css"
+  },
+  {
+    "revision": "d986da512d473a1d379d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.e5f65d05.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.e5f65d05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2f3030373d2bd8b2209",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.5bba0f56.chunk.js"
+  },
+  {
+    "revision": "9ed00262ab067fcfa080",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.5a6095d3.js"
+  }
+]);
